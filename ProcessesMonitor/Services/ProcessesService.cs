c21bef3513f4e23570aa1d5874b020d670001c19
v.kondratev <v.kondratev@ase-ec.ru>
2024-12-16 14:53:38 +0300

    removed useless init

--- ProcessesMonitor/Services/ProcessesService.cs
+++ ProcessesMonitor/Services/ProcessesService.cs
@@ -5,59 +5,21 @@ using ProcessesMonitor.Models;
 
 namespace ProcessesMonitor.Services;
 
-public class ProcessesService : IProcessesService
+public class ProcessesService(IHubContext<HighLoadWarningHub> hubContext, 
+    ILogger<ProcessesService> logger) : IProcessesService
 {
     private const string ReceiveMemoryHighLoad = "ReceiveMemoryHighLoad";
     private const string ReceiveCpuHighLoad = "ReceiveCpuHighLoad";
     private const long MbDivider = 1048576;
 
-    private readonly IHubContext<HighLoadWarningHub> _hubContext;
-    private readonly ILogger<ProcessesService> _logger;
-    private readonly Dictionary<int, ProcessEntity> _processDictionary;
-    
-    private readonly long _totalAvailableMemory;
-    private readonly double _processorCount;
+    private readonly Dictionary<int, ProcessEntity> _processDictionary = [];
+
+    private readonly long _totalAvailableMemory = GC.GetGCMemoryInfo().TotalAvailableMemoryBytes / MbDivider;
+    private readonly double _processorCount = Convert.ToDouble(Environment.ProcessorCount);
     private bool _isHighLoadCpu;
     private bool _isHighLoadMemory;
 
     public IReadOnlyCollection<ProcessEntity> Processes => _processDictionary.AsReadOnly().Values;
-
-    public ProcessesService(IHubContext<HighLoadWarningHub> hubContext, ILogger<ProcessesService> logger)
-    {
-        _hubContext = hubContext;
-        _logger = logger;
-
-        _totalAvailableMemory = GC.GetGCMemoryInfo().TotalAvailableMemoryBytes / MbDivider;
-        _processorCount = Convert.ToDouble(Environment.ProcessorCount);
-
-        _processDictionary = new Dictionary<int, ProcessEntity>();
-
-        Process[] processes = Process.GetProcesses();
-        
-        foreach (Process process in processes)
-        {
-            try
-            {
-                ProcessEntity processVm = new(process.Id, process.ProcessName)
-                {
-                    MemoryUsage = process.WorkingSet64 / MbDivider,
-                    OldTotalProcessorTime = process.TotalProcessorTime.TotalMilliseconds,
-                    LastTime = DateTime.Now,
-                    IsActive = true
-                };
-
-                _processDictionary.Add(process.Id, processVm);
-            }
-            catch (Win32Exception _)
-            {
-                // ignored
-            }
-            catch (Exception exception)
-            {
-                logger.LogError(exception.Message);
-            }
-        }
-    }
 
     public async Task UpdateProcessesAsync()
     {
@@ -82,7 +44,7 @@ public class ProcessesService : IProcessesService
             }
             catch (Exception exception)
             {
-                _logger.LogError(exception.Message);
+                logger.LogError(exception.Message);
             }
         }
 
@@ -133,15 +95,15 @@ public class ProcessesService : IProcessesService
             if (!_isHighLoadCpu)
             {
                 _isHighLoadCpu = true;
-                await _hubContext.Clients.All.SendAsync(ReceiveCpuHighLoad, _isHighLoadCpu);
-                _logger.LogWarning("Warning! CPU usage has exceeded 80%");
+                await hubContext.Clients.All.SendAsync(ReceiveCpuHighLoad, _isHighLoadCpu);
+                logger.LogWarning("Warning! CPU usage has exceeded 80%");
             }
         }
         else if (_isHighLoadCpu)
         {
             _isHighLoadCpu = false;
-            await _hubContext.Clients.All.SendAsync(ReceiveCpuHighLoad, _isHighLoadCpu);
-            _logger.LogInformation("CPU usage is less than 80%");
+            await hubContext.Clients.All.SendAsync(ReceiveCpuHighLoad, _isHighLoadCpu);
+            logger.LogInformation("CPU usage is less than 80%");
         }
         
         if (totalMemoryUsage/_totalAvailableMemory > 0.8)
@@ -149,15 +111,15 @@ public class ProcessesService : IProcessesService
             if (!_isHighLoadMemory)
             {
                 _isHighLoadMemory = true;
-                await _hubContext.Clients.All.SendAsync(ReceiveMemoryHighLoad, _isHighLoadMemory);
-                _logger.LogWarning("Warning! Available memory is running low!");
+                await hubContext.Clients.All.SendAsync(ReceiveMemoryHighLoad, _isHighLoadMemory);
+                logger.LogWarning("Warning! Available memory is running low!");
             }
         }
         else if (_isHighLoadMemory)
         {
             _isHighLoadMemory = false;
-            await _hubContext.Clients.All.SendAsync(ReceiveMemoryHighLoad, _isHighLoadMemory);
-            _logger.LogInformation("There is enough available memory!");
+            await hubContext.Clients.All.SendAsync(ReceiveMemoryHighLoad, _isHighLoadMemory);
+            logger.LogInformation("There is enough available memory!");
         }
     }
 }
